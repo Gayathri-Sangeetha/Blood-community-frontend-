5b2c1fde21d8cd7b39ddc6fc74df37afb1ee8b0b
Gayathri-Sangeetha <gayathrisangeetha28@gmail.com>
2025-12-15 21:45:38 +0530

    updated app.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Home from './pages/Home.jsx'
 import AdminLogin from './pages/AdminLogin.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
+import ButtonPage from './pages/ButtonPage.jsx';
+import DonarPage from './pages/DonarPage.jsx';
+import DonateDetails from './pages/DonateDetails.jsx';
+import RequesterPage from './pages/RequesterPage.jsx';
+import RequesterDetails from './pages/RequesterDetails.jsx';
 const App=()=>{
   return (
     <BrowserRouter>
@@ -11,6 +16,12 @@ const App=()=>{
       <Route path='admin-login' element={<AdminLogin/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/sign-up' element={<Signup/>}/>
+      <Route path='/button-page' element={<ButtonPage/>}/>
+      <Route path='/donar-dashboard' element={<DonarPage/>}/>
+      <Route path='/donation-details' element={<DonateDetails/>}/>
+      <Route path='/requester-dashboard' element={<RequesterPage/>}/>
+      <Route path="/requester-details/:donorEmail" element={<RequesterDetails />} />
+
     </Routes>
     </BrowserRouter>);}
 
